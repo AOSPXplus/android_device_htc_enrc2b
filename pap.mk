4f13ffe5ded46e3420e92bea150dcc2c90a8fc51
Rob Shannon <spirit2457@gmail.com>
2013-11-05 10:08:32 +0000

    Update pap.mk

--- pap.mk
+++ pap.mk
@@ -21,4 +21,4 @@ PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := One X+
 
 #Set build fingerprint / ID / Product Name etc.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=enrc2b BUILD_FINGERPRINT="htc_europe/enrc2b/enrc2b:4.3/JRO03C/125529.22:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.22 CL125529 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=enrc2b BUILD_FINGERPRINT="htc_europe/enrc2b/enrc2b:4.4/KRT16M/125529.22:user/release-keys" PRIVATE_BUILD_DESC="3.14.401.22 CL125529 release-keys"
